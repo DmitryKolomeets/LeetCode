4d575e5dd53ab6b67173f7a51ce53ef83f54b06c
Dmitry Kolomeets <bde@ukr.net>
2023-07-27 12:01:08 +0200

    Majority Element

---
+++ src/main/java/Easy/_169_Majority_Element/Solution.java
@@ -0,0 +1,27 @@
+package Easy._169_Majority_Element;
+
+public class Solution {
+
+
+    public int majorityElement(int[] nums) {
+        int cand = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (count == 0) {
+                cand = nums[i];
+            }
+
+            if (cand == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+
+        }
+        return cand;
+    }
+
+
+}
