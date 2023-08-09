8cc5dfa7f13d5cadfe143429cc6fca8748c1b528
Dmitry Kolomeets <bde@ukr.net>
2023-08-09 10:05:37 +0200

    1512 Number Of Good Pairs

---
+++ src/main/java/Easy/_1512_Number_Of_Good_Pairs/Solution.java
@@ -0,0 +1,25 @@
+package Easy._1512_Number_Of_Good_Pairs;
+
+public class Solution {
+
+
+    //    nums = [1,2,3,1,1,3]
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+       int[]  nums = {1,2,3,1,1,3};
+
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+}
