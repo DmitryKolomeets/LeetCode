18a7788acd9bbd86c845dbbbd1de256cde377343
Dmitry Kolomeets <bde@ukr.net>
2023-08-30 20:55:50 +0200

    171 Excel sheet column number 2

---
+++ src/main/java/Easy/_541_Reverse_String_ll/Solution.java
@@ -0,0 +1,23 @@
+package Easy._541_Reverse_String_ll;
+
+public class Solution {
+
+    public String reverseStr(String s, int k) {
+        int a_pointer = 0;
+        int b_pointer = s.length - 1;
+
+        while (a_pointer <= b_pointer) {
+
+            char temp = s[a_pointer];
+            s[a_pointer] = s[b_pointer];
+            s[b_pointer] = temp;
+
+            a_pointer++;
+            b_pointer--;
+        }
+    }
+
+
+
+
+}
